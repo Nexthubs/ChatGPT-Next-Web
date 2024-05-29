9bfcf996a48d734582b472bf154ec80058bac763
我是找找 <98395115+Nexthubs@users.noreply.github.com>
2024-05-29 20:05:49 +0800

    Update sidebar.tsx
    修改标题

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          Giaogiao GPT
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          猛男Giaogiao GPT.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
