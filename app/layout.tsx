514e4926631c0467f292b3eeb58f89c2558d331c
我是找找 <98395115+Nexthubs@users.noreply.github.com>
2025-02-07 15:43:02 +0800

    Update layout.tsx
    修改网页meta信息

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 import { getServerSideConfig } from "./config/server";
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Giaogiao GPT",
+  description: "猛男Giaogiao GPT.",
   appleWebApp: {
-    title: "NextChat",
+    title: "Giaogiao GPT",
     statusBarStyle: "default",
   },
 };
