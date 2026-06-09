a08280ad43107097d52416859b472157421f0062
我是找找 <98395115+Nexthubs@users.noreply.github.com>
2026-06-09 17:30:06 +0800

    Remove unnecessary closing div tag in sidebar
    Removed an unnecessary closing div tag.

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -256,7 +256,6 @@ export function SideBar(props: { className?: string }) {
         shouldNarrow={shouldNarrow}
       >
 
-        </div>
         {showDiscoverySelector && (
           <Selector
             items={[
